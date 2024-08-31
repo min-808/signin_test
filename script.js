8bc909b82071d9b514d5c4d8489d8355603a7ebd
Min <kendamamin1480@gmail.com>
2024-08-31 10:23:55 -1000

    Added incorrect pass option

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-async function signIn(data) {
+async function signIn(user, pass) {
     try {
         const response = await fetch('https://nodejs-serverless-function-express-one-teal.vercel.app/api/test', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username: data }),
+            body: JSON.stringify({ username: user, password: pass }),
         })
         
-        if (response.ok) {
+        if (response.ok) { // Ok, 200-299 status code
             const comeback = await response.json()
 
             if (comeback["status"] = "Success") {
                 console.log("Successfully signed in as user: " + comeback["username"])
+            } else if (comeback["status"] = "Incorrect") {
+                var error = document.createElement("p")
+                error.innerHTML = "Incorrect username or password"
+                document.getElementById("password_input").appendChild(error)
             } else {
                 console.log("Something went wrong")
             }
