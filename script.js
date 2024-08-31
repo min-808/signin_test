2f91ba16b02a91e407bc327e62562f841a7f002d
Min <kendamamin1480@gmail.com>
2024-08-31 00:29:49 -1000

    added header

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ async function signIn(data) {
     try {
         const response = await fetch('https://nodejs-serverless-function-express-one-teal.vercel.app/api/test', {
             method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
             body: JSON.stringify({ username: data }),
         })
         
