10ff5b3affdb7364e8ef10d8a961d330c110eea2
Min <kendamamin1480@gmail.com>
2024-09-21 23:27:37 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ async function signIn(user, pass) {
     try {
         const response = await fetch('https://nodejs-serverless-function-express-one-teal.vercel.app/api/test', {
             method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
             body: JSON.stringify({ username: user, password: pass }),
         })
         
